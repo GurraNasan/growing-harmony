14d2b6e227c21dc8849301b1d39674d2e9019946
GurraNasan <gurra.naslund@gmail.com>
2022-12-21 13:20:08 +0000

    Add model for post to forum

--- forum/models.py
+++ forum/models.py
@@ -14,3 +14,23 @@ class Categories(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    categorie_image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    exerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+    class Meta:
+        # Arrange the post by when they are created  
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return str(self.title)
