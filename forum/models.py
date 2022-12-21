c9420f0ae3020a15b4b40f20830a3229e405f6a1
GurraNasan <gurra.naslund@gmail.com>
2022-12-21 13:00:43 +0000

    Build models for categories

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    categorie_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        # Arrange the categories after title
+        ordering = ['title']
+
+    def __str__(self):
+        return str(self.title)
