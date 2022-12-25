a0ac4a7df9e095c5c189688e9f9c69f08afe6c7e
GurraNasan <gurra.naslund@gmail.com>
2022-12-25 16:02:36 +0000

    Add category to forum so user can see them in the page

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('forum/', views.CategoriesList.as_view(), name='forum'),
-    path('contact', views.contact, name='contact')
+    path('forum/', views.CategoryList.as_view(), name='forum'),
+    path('contact/', views.contact, name='contact')
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.views import generic, View
-from django.http import HttpResponse
 from .models import Categories
 from .models import Post
-
-
-class CategoriesList(generic.ListView):
-    model = Categories
-    template_name = 'forum.html'
-    paginate_by = 8
 
 
 def home(request):
     return render(request, 'index.html')
 
+
+class CategoryList(generic.ListView):
+    model = Categories
+    template_name = 'forum.html'
+    queryset = Categories.objects.all()
+
+
+# class PostList(generic.ListView):
+#    model = Post
+#    queryset = Post.objects.order_by('-created_on')
+#    template_name = 'forum.html'
 
 def contact(request):
     return render(request, 'contact.html')
