d37dfc61c82a7a80ad76f65b37831662d181b3db
GurraNasan <gurra.naslund@gmail.com>
2022-12-29 13:02:56 +0000

    deleted the category_img in the post model

--- forum/models.py
+++ forum/models.py
@@ -19,7 +19,6 @@ class Categories(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    categorie_image = CloudinaryField('image', default='placeholder')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
