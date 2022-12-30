e8dd135c7387d04caf62e9b2c6149c7e74334b79
GurraNasan <gurra.naslund@gmail.com>
2022-12-30 11:38:54 +0000

    Add style to signup, login and register button and fixed a bug with the contact page

--- forum/urls.py
+++ forum/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
     path('forum/', views.CategoryList.as_view(), name='forum'),
     path('<slug:slug>/', views.CategoryDetail.as_view(), name='category_detail'),
-    path('contact/', views.contact, name='contact'),
 ]
