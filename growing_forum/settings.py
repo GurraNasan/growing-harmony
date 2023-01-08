4d7c813fbf9fa5f674b7102e072d6d5ad2b37d1b
GurraNasan <gurra.naslund@gmail.com>
2023-01-08 12:15:45 +0000

    deployment commit

--- growing_forum/settings.py
+++ growing_forum/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORGIN'
 
 ALLOWED_HOSTS = ["growingharmoni.herokuapp.com/", "localhost"]
 ACCOUNT_EMAIL_VERIFICATION = 'none'
